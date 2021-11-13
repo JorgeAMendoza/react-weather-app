1c338e13211b896014ec22164cc3228c216cb6b8
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-11-13 08:20:22 -0600

    create states for the weather data, passing state values and set states into header component with search bar

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,23 @@
-
+import { useState } from 'react';
+import { Header } from './components/Header/Header';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [unit, setUnit] = useState('F');
+  const [weatherData, setWeatherData] = useState('');
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header
+        search={searchQuery}
+        setSearch={setSearchQuery}
+        setWeather={setWeatherData}
+        unitType={unit}
+        setUnitType={setUnit}
+      />
+
+      {/* Extract the current weather Object and place into teh Current weather Component */}
+      {/* Extractg the forecat object and place into the forecat component.  */}
+    </>
   );
 }
 
