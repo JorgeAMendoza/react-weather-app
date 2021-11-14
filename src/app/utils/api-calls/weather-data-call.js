f3e43ee077d389375d720ff4b7b140f5fae8dfe6
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-11-14 09:02:27 -0600

    use geo location information to make call to OPenWeather API
    application can call API and get all weather infomration about current waether and forecast weather for the next 7 days

--- src/app/utils/api-calls/weather-data-call.js
+++ src/app/utils/api-calls/weather-data-call.js
@@ -1,1 +1,13 @@
-export const weatherDataCall = (geoData) => {};
+export const weatherDataCall = async (lat, lon, unit) => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const searchUnit = unit === 'F' ? 'imperial' : 'metric';
+
+  const weatherResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts,hourly&units=${searchUnit}&appid=${API_KEY}`
+  );
+
+  const weatherData = await weatherResponse.json();
+  console.log(weatherData);
+  //   So now we need to grab the current weather data and the daily weahther data.
+  //   So here we will extract teh current and forecast data, and pass them into a class/factory that allows us to grab and modify the data through methods.
+};
