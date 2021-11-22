23b9cd61e6ee6aa0d75a1ecd75ead5c1cb6d0dde
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-11-22 11:48:51 -0600

    components can now display data retrieved from API call

--- src/app/components/Weather Forecast/ForecastCard.js
+++ src/app/components/Weather Forecast/ForecastCard.js
@@ -1,11 +1,24 @@
-export const ForecastCard = ({ min, max, date, iconID, description }) => {
+export const ForecastCard = ({
+  min,
+  max,
+  date,
+  iconID,
+  outlook,
+  weatherID,
+}) => {
   return (
     <div>
       {/* Icon */}
       {/* <Day>Example Date</Day>
       <Outlook>Status</Outlook>
       <Temps /> */}
+
+      <p>{min}</p>
+      <p>{max}</p>
+      <p>{outlook}</p>
+      <p>{iconID}</p>
       <p>{date}</p>
+      <p>{weatherID}</p>
     </div>
   );
 };
--- src/app/components/Weather Forecast/ForecastCards.js
+++ src/app/components/Weather Forecast/ForecastCards.js
@@ -3,20 +3,30 @@ import { ForecastCard } from './ForecastCard';
 
 export const ForecastCards = ({ weatherData }) => {
   const dateString = DateString();
+  console.log(weatherData);
 
-  // const createForecastCards = (data) => {
-  //   const dateObject = new Date();
-  //   let dateIterator = 0;
-  //   return data.map((info) => {
-  //     const cardDate = dateString.dateToString(dateObject.setDate() + dateIterator);
-  //     dateIterator++;
-  //     return <ForecastCard key={cardDate} date={cardDate} />;
-  //   });
-  // };
+  const createForecastCards = (data) => {
+    const dateObject = new Date();
+    return data.map((info) => {
+      dateObject.setDate(dateObject.getDate() + 1);
+      const cardDate = dateString.dateToString(dateObject);
+      return (
+        <ForecastCard
+          key={cardDate}
+          date={cardDate}
+          min={info.min}
+          max={info.max}
+          iconID={info.iconID}
+          outlook={info.outlook}
+          weatherID={info.weatherID}
+        />
+      );
+    });
+  };
   return (
     <div>
       <p>Hello</p>
-      {/* {createForecastCards(weatherData)} */}
+      {createForecastCards(weatherData)}
     </div>
   );
 };
