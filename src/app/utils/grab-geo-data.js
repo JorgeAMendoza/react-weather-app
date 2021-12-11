d24836011703afe7c5bd30f909f0241ae94d7c06
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-12-11 10:46:09 -0600

    fixed issue where city would not be found if state acronym was passed in
    api change made it so the full state name was returned instaed of the acronym, so i updated the grabGeoData function to mathc with a full state name (grabbed with the diciontary) instead of the acronym

--- src/app/utils/grab-geo-data.js
+++ src/app/utils/grab-geo-data.js
@@ -1,12 +1,16 @@
+import { stateDictionary } from './api-calls/state-dictionary';
+
 export const grabGeoData = (geoData, city, state) => {
+  if (geoData.length === 1) return geoData[0];
   if (geoData.length === 0) throw new Error('City Not Found');
   if (!state) return geoData[0];
 
   for (let location of geoData) {
-    if (!location.hasOwnProperty('state')) continue;
+    console.log(location.name, city);
+    console.log(location.state, stateDictionary[state]);
     if (
       location.name.toUpperCase() === city.toUpperCase() &&
-      location.state.toUpperCase() === state.toUpperCase()
+      location.state.toUpperCase() === stateDictionary[state].toUpperCase()
     )
       return location;
   }
