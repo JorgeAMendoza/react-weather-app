40c7b2857c44534201546198eeb235e58b827c08
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-12-11 11:04:27 -0600

    pass in string to the styled component instead of a boolean

--- src/app/components/ErrorModal/ErrorModal.js
+++ src/app/components/ErrorModal/ErrorModal.js
@@ -3,7 +3,7 @@ import { ErrorModalStyled } from '../../styles/ErrorModal/ErrorModal.styled';
 export const ErrorModal = ({ errorMessage, show }) => {
   const message = errorMessage;
   return (
-    <ErrorModalStyled display={show}>
+    <ErrorModalStyled display={show ? 'true' : 'false'}>
       <p aria-live="polite">{message}</p>
     </ErrorModalStyled>
   );
