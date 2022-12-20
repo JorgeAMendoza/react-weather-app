10d674a7174220fd8bf213b6879302ebfdde7c47
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-12-20 10:59:27 -0600

    bootstrap the project with vite, configure the project to use cypress, use eslint, and install variuos dependencies, mainly tanstack query

--- src/app/components/Current Weather/Temperature.js
+++
@@ -1,5 +0,0 @@
-import { TemperatureStyled } from '../../styles/CurrentWeather/Temperature.styled';
-
-export const Temperature = ({ temperature }) => {
-  return <TemperatureStyled>{temperature} °</TemperatureStyled>;
-};
--- src/app/components/Weather Forecast/ForecastCard/ForecastOutlook.js
+++
@@ -1,9 +0,0 @@
-import { ForecastOutlookStyled } from '../../../styles/Weather Forecast/ForecastCard/ForecastOutlook.styled';
-
-export const ForecastOutlook = ({ outlook }) => {
-  return (
-    <ForecastOutlookStyled>
-      <p>{outlook}</p>
-    </ForecastOutlookStyled>
-  );
-};
