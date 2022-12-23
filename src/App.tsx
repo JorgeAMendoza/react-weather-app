3978369848e18ac385e97f2a398ccbedca592548
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-12-23 08:51:40 -0600

    install react hook form, setup the use form hook for the search bar component, regex pattern used to only search for a city, or a city and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   // state to keep track of farheheit, and celcius
@@ -7,7 +8,11 @@ function App() {
   // variable that extracts the forecast data
   // varaible that extracts teh day data
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,46 @@
-// most likely will take in mutatio to fire off
+import { useForm } from 'react-hook-form';
+import searchIcon from '../../assets/imgs/search.svg';
+import { SubmitHandler } from 'react-hook-form/dist/types';
+// simple form, so just put all the form content here
+
+interface SearchFormProps {
+  searchQuery: string;
+}
 
 const SearchBar = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SearchFormProps>({
+    defaultValues: {
+      searchQuery: '',
+    },
+  });
+
+  console.log(errors.searchQuery);
+
+  const searchGeoLocation: SubmitHandler<SearchFormProps> = (data) =>
+    console.log('this is dat', data);
+
   return (
-    <div>
-      <p>this will be the search bar</p>
-    </div>
+    <header>
+      <div>
+        <div>
+          <img src={searchIcon} alt="" />
+        </div>
+        <form onSubmit={handleSubmit(searchGeoLocation)}>
+          <input
+            type="text"
+            {...register('searchQuery', {
+              pattern:
+                /^[A-Za-z.' ]+$|^[A-Za-z.' ]+$|^[A-Za-z.' ]+, [A-Za-z]+$/gi,
+            })}
+            aria-invalid={errors.searchQuery ? 'true' : 'false'}
+          />
+        </form>
+      </div>
+    </header>
   );
 };
 
