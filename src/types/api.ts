63b9d67b985135ef242aeb53aa6c0a8561a5d7cd
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-12-29 08:39:12 -0600

    create types for the api response for current weather and forecast

--- src/types/api.ts
+++ src/types/api.ts
@@ -7,5 +7,47 @@ interface GeoData {
   state: string;
 }
 
-// api is set to respond with only one geo data, so response will be a single tuple of geo data
+export interface CurrentWeather {
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  main: {
+    temp: number;
+    temp_min: number;
+    temp_max: string;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+}
+
+interface ForecastWeather {
+  temp: {
+    min: number;
+    max: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: '10d';
+    }
+  ];
+}
+
+export interface WeatherData {
+  city: string;
+  location: string;
+  currentWeather: CurrentWeather;
+  forcastWeather: ForecastWeather[];
+}
+
 export type GeoDataResponse = [GeoData];
