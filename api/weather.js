948ead25bba08a6e1953413006cc8d512eef800b
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-12-31 11:10:33 -0600

    call to one weather api can be completed with lon and lat params, error is returned if no geo data is found, currently no error implemented yet

--- api/weather.js
+++ api/weather.js
@@ -4,9 +4,22 @@ import axios from 'axios';
 export default async function fetchWeather(request, response) {
   const geoDataAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${request.query.location}&limit=1&appid=${process.env.API_KEY}`;
 
-  const { data } = await axios.get(geoDataAPI);
+  const { data: geoResponse } = await axios.get(geoDataAPI);
+
+  if (geoResponse.length === 0) {
+    console.log('returning error');
+    return response.status(404).json({
+      error: 'city not found',
+    });
+  }
+
+  const { lat, lon } = geoResponse[0];
+
+  const oneWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts,hourly&appid=${process.env.API_KEY}`;
+
+  const { data: weatherResponse } = await axios.get(oneWeather);
 
   response.status(200).json({
-    data: data,
+    data: weatherResponse,
   });
 }
