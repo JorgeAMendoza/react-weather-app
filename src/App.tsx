746bdae52ba02cc6d68643971e8d90c1f09006fe
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-01-09 05:34:22 -0600

    create state to keep track of which unit to display, button in header flips between C or F on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
-import { QueryClient, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { OneWeatherCall } from './types/api';
 import fetchWeatherData from './utils/fetch-weather-data';
 
 function App() {
   const [search, setSearch] = useState('Dallas, Texas');
+  const [unit, setUnit] = useState<'F' | 'C'>('F');
   const queryClient = useQueryClient();
   const { data, error } = useQuery<OneWeatherCall, Error>({
     queryKey: ['weather', search],
@@ -20,9 +21,17 @@ function App() {
     },
   });
 
+  // here, we need to create a function that will change our weather data for us, meaning it will either return the object but with metric/imperial/ or an undefined array, undefined is if we are looking for data, maybe, still gotta figure that out.
+
   return (
     <div className="App">
-      <SearchBar setSearch={setSearch} />
+      <header>
+        <SearchBar setSearch={setSearch} />
+        <button onClick={() => (unit === 'F' ? setUnit('C') : setUnit('F'))}>
+          {unit}°
+        </button>
+      </header>
+
       {error && <p>{error.message}</p>}
     </div>
   );
