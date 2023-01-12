745ce74fb7793d0962c038646e298fe52d755a55
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-01-12 05:35:24 -0600

    move logic for conversion into seperate function which returns the current weather and forecast objects

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import SearchBar from './components/SearchBar/SearchBar';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { OneWeatherCall, WeatherData } from './types/api';
 import fetchWeatherData from './utils/fetch-weather-data';
-import imperialConvert from './utils/standard-to-imperial';
-import metricConvert from './utils/standard-to-metric';
+import convertUnits from './utils/convert-units';
 
 function App() {
   const [search, setSearch] = useState('Dallas, Texas');
@@ -25,55 +24,11 @@ function App() {
   const weatherData: undefined | WeatherData = useMemo(() => {
     console.log('i am called');
     if (!weatherCall) return undefined;
-    const { temp, feels_like, min_temp, max_temp, wind_speed, humidity } =
-      weatherCall.data.current;
-    const forecastWeather = weatherCall.data.weekForecast;
-
-    const current: WeatherData['current'] = {
-      temp:
-        unit === 'F'
-          ? imperialConvert.convertTemp(temp)
-          : metricConvert.convertTemp(temp),
-      feels_like:
-        unit === 'F'
-          ? imperialConvert.convertTemp(feels_like)
-          : metricConvert.convertTemp(feels_like),
-      min_temp:
-        unit === 'F'
-          ? imperialConvert.convertTemp(min_temp)
-          : metricConvert.convertTemp(min_temp),
-      max_temp:
-        unit === 'F'
-          ? imperialConvert.convertTemp(max_temp)
-          : metricConvert.convertTemp(max_temp),
-      wind_speed:
-        unit === 'F'
-          ? imperialConvert.convertWind(wind_speed)
-          : Math.round(wind_speed),
-      humidity,
-    };
-
-    const weekForecast: WeatherData['weekForecast'] = forecastWeather.map(
-      ({ temp, humidity, weather }) => ({
-        temp: {
-          day:
-            unit === 'F'
-              ? imperialConvert.convertTemp(temp.day)
-              : metricConvert.convertTemp(temp.day),
-          min:
-            unit === 'F'
-              ? imperialConvert.convertTemp(temp.min)
-              : metricConvert.convertTemp(temp.min),
-          max:
-            unit === 'F'
-              ? imperialConvert.convertTemp(temp.max)
-              : metricConvert.convertTemp(temp.max),
-        },
-        humidity: humidity,
-        weather: weather,
-      })
+    const { current, weekForecast } = convertUnits(
+      weatherCall.data.current,
+      weatherCall.data.weekForecast,
+      unit
     );
-
     return {
       city: weatherCall.data.city,
       location: weatherCall.data.location,
