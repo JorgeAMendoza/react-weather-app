924e80568ace0c77e688e300d2702922d1e46b81
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-01-17 05:41:53 -0600

    create global style for the application

---
+++ src/styles/Global.styled.tsx
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root{
+    --white: #fff;
+    --black: #000;
+    --blue: #005A8E
+    --light-blue: #00A2FF;
+  }
+  html {
+    font-size: 62.5%;
+    height:100%;
+  }
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    line-height: 1.4;
+    z-index:1;
+  }
+  body{
+    font-family: 'Lato', sans-serif;
+    line-height:1.5;
+    font-size: 1.6rem;
+    min-height: 100%;
+    position:relative;
+    background-color: var(--dark-blue);
+    color: var(--white);
+  } 
+  img,svg,picture,video,canvas {
+    max-width: 100%;
+    display: block;
+  }
+  a{
+    color:white;
+    text-decoration:none;
+  }
+  input,textarea,select,button {
+    font-family: inherit;
+  }
+  ul,ol{
+    list-style: none;
+  }
+  p,
+  li,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+  }
+
+  #root, #__next {
+    isolation: isolate;
+  }
+`;
+
+export default GlobalStyle;
