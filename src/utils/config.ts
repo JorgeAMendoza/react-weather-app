52862e9c2c19d524098aec9758ff09798a0b93e5
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-01-28 10:05:09 -0600

    set the fetch weather function to use the current host it is on to fire the api request, removed the config file

--- src/utils/config.ts
+++
@@ -1,1 +0,0 @@
-export const API_KEY = import.meta.env.VITE_API_KEY;
--- src/utils/fetch-weather-data.ts
+++ src/utils/fetch-weather-data.ts
@@ -1,18 +1,14 @@
 import axios from 'axios';
 import { OneWeatherCall } from '../types/api';
-
 const fetchWeatherData = async (
   searchQuery: string
 ): Promise<OneWeatherCall> => {
   try {
-    const { data } = await axios.get<OneWeatherCall>(
-      'http://localhost:3000/api/weather',
-      {
-        params: {
-          location: searchQuery,
-        },
-      }
-    );
+    const { data } = await axios.get<OneWeatherCall>(`/api/weather`, {
+      params: {
+        location: searchQuery,
+      },
+    });
 
     return data;
   } catch (e) {
